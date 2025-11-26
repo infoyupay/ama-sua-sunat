9059084d277dd6cba222d96e6717ed9640a1513a
dvidal <dvidal@infoyupay.com>
2025-11-25 23:39:31 -0500

    feat(build): configure jOOQ plugin and generation task in Gradle
    Add full jOOQ code generation support to the fv3800 module via the
    `nu.studer.jooq` Gradle plugin.
    
    Key additions:
    - jOOQ plugin application and version alignment with project dependencies
    - datasource settings for schema introspection
    - definition of the `generateJooq` task and its target directories
    - integration of generated sources into the FV-3800 build workflow
    - package-level organization for generated DSL and records
    
    This lays the groundwork for type-safe SQL access in the module and allows
    automatic regeneration of jOOQ classes from schema changes.

--- fv3800/build.gradle.kts
+++ fv3800/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jooq.meta.jaxb.ForcedType
+
 plugins {
     // Standalone exec
     application
@@ -7,6 +9,8 @@ plugins {
     id("org.beryx.jlink") version "3.1.3"
     // Java support
     java
+    // jooq codegen
+    id("nu.studer.jooq") version "10.1.1"
 }
 
 java {
@@ -19,7 +23,19 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir("src/jooq/java")
+        }
+    }
+}
+
+
 dependencies {
+    // --- jooq codegen deps
+    jooqGenerator("org.xerial:sqlite-jdbc:3.51.0.0")
+
     // --- Runtime & DB ---
     implementation("org.xerial:sqlite-jdbc:3.51.0.0")
 
@@ -28,6 +44,9 @@ dependencies {
 
     // ValidatorFX (JavaFX forms)
     implementation("net.synedra:validatorfx:0.6.3")
+
+    // https://mvnrepository.com/artifact/org.jetbrains/annotations
+    implementation("org.jetbrains:annotations:26.0.2-1")
 
     // --- Testing ---
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
@@ -52,6 +71,113 @@ javafx {
         "javafx.base"
     )
 }
+
+jooq {
+    version.set("3.20.9")
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+
+                // JDBC Config
+                jdbc.apply {
+                    driver = "org.sqlite.JDBC"
+                    url = "jdbc:sqlite:$projectDir/model/amasua-3800.db"
+                }
+
+                generator.apply {
+
+                    // DB config
+                    database.apply {
+                        name = "org.jooq.meta.sqlite.SQLiteDatabase"
+                        //inputSchema = "main"
+
+                        // ----------- FORCED TYPES -----------
+                        forcedTypes = listOf(
+
+                            // BOOLEAN FIELDS
+                            ForcedType().apply {
+                                name = "BOOLEAN"
+                                includeTypes = "INTEGER"
+                                includeExpression = ".*\\.(is_.*|has_.*|active|resident)"
+                            },
+
+                            // ENUM: ENTITY_SUBTYPE
+                            ForcedType().apply {
+                                name = "VARCHAR"
+                                includeExpression = ".*\\.entity_subtype"
+                                userType = "com.yupay.amasua.fv3800.model.enums.EntitySubtype"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.EntitySubtypeConverter"
+                            },
+
+                            // ENUM: CODE_LIST
+                            ForcedType().apply {
+                                name = "VARCHAR"
+                                includeExpression = ".*\\.list_id"
+                                userType = "com.yupay.amasua.fv3800.model.enums.ListIds"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.ListIdsConverter"
+                            },
+
+                            // ENUM: DOI_TYPE
+                            ForcedType().apply {
+                                name = "VARCHAR"
+                                includeExpression = ".*\\.[a-z_]*doi_type$"
+                                userType = "com.yupay.amasua.fv3800.model.enums.DoiType"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.DoiTypeConverter"
+                            },
+
+                            // ENUM: MARITAL_STATUS
+                            ForcedType().apply {
+                                name = "VARCHAR"
+                                includeExpression = ".*\\.marital_status"
+                                userType = "com.yupay.amasua.fv3800.model.enums.MaritalStatus"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.MaritalStatusConverter"
+                            },
+
+                            // ENUM: INDIRECT_TYPE
+                            ForcedType().apply {
+                                name = "VARCHAR"
+                                includeExpression = ".*\\.indirect_type"
+                                userType = "com.yupay.amasua.fv3800.model.enums.IndirectType"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.IndirectTypeConverter"
+                            },
+
+                            // DATE: of epoch day
+                            ForcedType().apply {
+                                name = "INTEGER"
+                                includeExpression = ".*\\.(birthday|.*_date|.*_at)$"
+                                userType = "java.time.LocalDate"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.EpochDaysConverter"
+                            },
+
+                            // UNSCALED DECIMALS: integers, 6 decimals
+                            ForcedType().apply {
+                                name = "INTEGER"
+                                includeExpression = ".*\\.(value_.*|.*_percentage)$"
+                                excludeExpression = ".*\\.value_type"
+                                userType = "java.math.BigDecimal"
+                                converter = "com.yupay.amasua.fv3800.model.jooq.converters.UnscaledDecimal6"
+                            }
+                        )
+                    }
+
+                    // Output target
+                    target.apply {
+                        packageName = "com.yupay.amasua.fv3800.model.jooq"
+                        directory = "$projectDir/src/jooq/java"
+                    }
+
+                    // POJO generation
+                    generate.apply {
+                        isPojos = true
+                        isDaos = false
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 jlink {
     // Runtime image name
